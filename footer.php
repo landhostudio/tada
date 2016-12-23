81f12d959fd8a7ba45478c265e9e612933f3876d
h0r0man <roman.horokhovatskyy@gmail.com>
2016-12-23 17:09:58 +0100

    Added footer semantics

--- footer.php
+++ footer.php
@@ -1,6 +1,23 @@
-    <footer role="contentinfo">
-      <p>&copy; 2014 – <?php echo date('Y'); ?> <?php bloginfo('name'); ?>. <?php esc_html_e('All rights reserved', 'horoman'); ?></p>
+    <footer role="contentinfo" class="footer">
+
+      <?php if (get_field('options_footer_icon', option)): ?>
+        <div class="footer__icon">
+          <img src="<?php the_field('options_footer_icon', option); ?>" alt="">
+        </div>
+      <?php endif; ?>
+
+      <?php if (get_field('options_footer_body', option)): ?>
+        <div class="footer__body">
+          <?php the_field('options_footer_body', option); ?>
+        </div>
+      <?php endif; ?>
+
+      <div class="footer__copyright">
+        <p>&copy; <?php $currentYear = date('Y'); if ($currentYear == $currentYear): ?><?php echo date('Y'); ?><?php else: ?>2016 – <?php echo date('Y'); ?><?php endif; ?> <?php bloginfo('name'); ?>.<?php if (get_field('options_footer_copyright', option)): ?> <?php the_field('options_footer_copyright', option); ?><?php endif; ?></p>
+      </div>
+
     </footer>
+
     <?php wp_footer(); ?>
   </body>
 </html>
