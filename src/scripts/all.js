9b752669b5b018f99125ef47e564ff0f96233e0b
h0r0man <roman.horokhovatskyy@gmail.com>
2017-01-03 12:53:30 +0100

    Added header scripts

--- src/scripts/all.js
+++ src/scripts/all.js
@@ -8,9 +8,28 @@ $(function() {
   'use strict';
 
   var init = function() {
+    initHeader();
+    initHeaderCTA();
     initNewsletter();
   };
-  
+
+  function initHeader() {
+    $('.header__menu button').click(function(event) {
+      $('body').toggleClass('header--opened')
+    });
+  };
+
+  function initHeaderCTA() {
+    $('.header__cta button').click(function(event) {
+
+      $('html, body').animate({
+        scrollTop: $('.form').offset().top
+      }, 2000 );
+
+      $('#email').focus();
+    });
+  };
+
   function initNewsletter() {
     
     $('.form__input form').ajaxChimp({
