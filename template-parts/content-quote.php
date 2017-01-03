31c2bc2c3dda9879ea03724231c10cab7fc5c75e
h0r0man <roman.horokhovatskyy@gmail.com>
2017-01-03 13:38:04 +0100

    Added quote

--- template-parts/content-quote.php
+++ template-parts/content-quote.php
@@ -1,18 +1,20 @@
-<?php if (get_sub_field('landing_sections_quote_title')): ?>
-  <section class="quote"<?php if (get_sub_field('landing_sections_quote_color') || get_sub_field('landing_sections_quote_background')): ?> style="<?php if (get_sub_field('landing_sections_quote_color')): ?>background-color: <?php the_sub_field('landing_sections_quote_color'); ?>;<?php endif; ?><?php if (get_sub_field('landing_sections_quote_background')): ?> background-image: url('<?php the_sub_field('landing_sections_quote_background'); ?>');<?php endif; ?>"<?php endif; ?>>
+<?php
+  $image      = get_sub_field('landing_sections_quote_background_image');
+  $size       = 'large';
+  $thumbLarge = $image['sizes'][ $size ];
+?>
 
-    <div class="quote__title">
+<?php if (get_sub_field('landing_sections_quote_title')): ?>
+  <section class="quote"<?php if (get_sub_field('landing_sections_quote_text_color') || get_sub_field('landing_sections_quote_background_color') || get_sub_field('landing_sections_quote_background_image')): ?> style="<?php if (get_sub_field('landing_sections_quote_text_color')): ?> color: <?php the_sub_field('landing_sections_quote_text_color'); ?>;<?php endif; ?><?php if (get_sub_field('landing_sections_quote_background_color')): ?> background-color: <?php the_sub_field('landing_sections_quote_background_color'); ?>;<?php endif; ?><?php if (get_sub_field('landing_sections_quote_background_image')): ?> background-image: url('<?php echo $thumbLarge; ?>');<?php endif; ?>"<?php endif; ?>>
+    <div class="quote__container">
       <h2><?php the_sub_field('landing_sections_quote_title'); ?></h2>
     </div>
 
-    <?php if (get_sub_field('landing_sections_quote_author') && get_sub_field('landing_sections_quote_author_image')): ?>
-
+    <?php if (get_sub_field('landing_sections_quote_author') && $image): ?>
       <div class="quote__author">
-        <p class="hidden"><?php the_sub_field('landing_sections_quote_author'); ?></p>
+        <span class="hidden"><?php the_sub_field('landing_sections_quote_author'); ?></span>
         <img src="<?php the_sub_field('landing_sections_quote_author_image'); ?>" alt="">
       </div>
-
     <?php endif; ?>
-
   </section>
 <?php endif; ?>
