83bd4f2a1b970c1925f4018f3c9862b04061521a
h0r0man <roman.horokhovatskyy@gmail.com>
2017-01-04 16:59:52 +0100

    Added site icons

---
+++ inc/icons.php
@@ -0,0 +1,4 @@
+<link rel="apple-touch-icon" sizes="180x180" href="<?php echo get_stylesheet_directory_uri(); ?>/dist/img/apple-touch-icon.png">
+<link rel="icon" type="image/png" href="<?php echo get_stylesheet_directory_uri(); ?>/dist/img/favicon-32.png" sizes="32x32">
+<link rel="icon" type="image/png" href="<?php echo get_stylesheet_directory_uri(); ?>/dist/img/favicon-16.png" sizes="16x16">
+<meta name="theme-color" content="#ffffff">
