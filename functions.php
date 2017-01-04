e6a87fcb065084d1c414d3aac6ef6faa1c05faec
h0r0man <roman.horokhovatskyy@gmail.com>
2017-01-04 13:36:28 +0100

    Temporally disabled asse versioning

--- functions.php
+++ functions.php
@@ -33,7 +33,7 @@
       // Soil ------------------------------------------------------------------
       
       add_theme_support('soil-clean-up');
-      add_theme_support('soil-disable-asset-versioning');
+      // add_theme_support('soil-disable-asset-versioning');
       add_theme_support('soil-disable-trackbacks');
       add_theme_support('soil-nav-walker');
       add_theme_support('soil-relative-urls');
