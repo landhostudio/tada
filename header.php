98ea9396d624921e2f394b4df6cff156707fb7f2
h0r0man <roman.horokhovatskyy@gmail.com>
2017-01-05 16:53:43 +0100

    Disabled pinterest pints for website

--- header.php
+++ header.php
@@ -4,6 +4,7 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="pinterest" content="nopin">
     <?php wp_head(); ?>
     <?php get_template_part('inc/icons'); ?>
   </head>
