880c1e61f2a6b93381e82d3d3efd32d7ea8c026a
h0r0man <roman.horokhovatskyy@gmail.com>
2017-03-01 16:36:14 +0100

    Updated front-end assets

--- functions.php
+++ functions.php
@@ -21,16 +21,15 @@
       // Load the assets -------------------------------------------------------
 			
       function init_assets() {
-        wp_enqueue_style('all-css', get_template_directory_uri() . '/dist/css/all.css', array(), '1.0.0');
-        wp_enqueue_script('plugins', get_template_directory_uri() . '/dist/js/plugins.js', array('jquery'), '1.0.0', true);
-        wp_enqueue_script('scripts', get_template_directory_uri() . '/dist/js/scripts.js', array('jquery'), '1.0.0', true);
+        wp_enqueue_style('all-css', get_template_directory_uri() . '/dist/css/all.css', array(), '1.5.0');
+        wp_enqueue_script('plugins', get_template_directory_uri() . '/dist/js/plugins.js', array('jquery'), '1.5.0', true);
+        wp_enqueue_script('scripts', get_template_directory_uri() . '/dist/js/scripts.js', array('jquery'), '1.5.0', true);
       }
       add_action('wp_enqueue_scripts', 'init_assets');
       
       // Soil ------------------------------------------------------------------
       
       add_theme_support('soil-clean-up');
-      // add_theme_support('soil-disable-asset-versioning');
       add_theme_support('soil-disable-trackbacks');
       add_theme_support('soil-nav-walker');
       add_theme_support('soil-relative-urls');
